618d04f06947a252a99e78404a04673b5d841938
liaojinhua1 <Jin9Code@163.com>
2019-12-12 10:14:43 +0800

    01-项目的基本结构和外部资源的引入，还没有开始任何的业务

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    aa
+   <!-- 在跟组件中只需要添加路由映射 -->
+   <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url('http://at.alicdn.com/t/font_1426139_h6vn3jbl5q.css');
 </style>
